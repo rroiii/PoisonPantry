b382aa31231abd19c572d35643761ee4e012c1b8
Roy Oswaldha <royoswaldha@gmail.com>
2023-11-24 14:54:09 +0700

    Change database name

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-$koneksi = mysqli_connect("localhost", "root", "", "fatalflora");
+$koneksi = mysqli_connect("localhost", "root", "", "PoisonPantry");
 if (mysqli_connect_errno()) {
     echo "Database connection failed : " . mysqli_connect_error();
 }
