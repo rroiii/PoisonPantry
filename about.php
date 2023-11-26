500062d21425fe7759531799e6c6129527c41410
Roy Oswaldha <royoswaldha@gmail.com>
2023-11-27 01:09:37 +0700

    Create about.php

---
+++ about.php
@@ -0,0 +1,73 @@
+<?php
+// File: download_brosur.php
+
+if (isset($_GET['brosur'])) {
+    $brosur = $_GET['brosur'];
+
+    // Menggunakan nilai $brosur langsung untuk menentukan path file
+    $brosurPath = $brosur;
+
+    if (file_exists($brosurPath)) {
+        // Mendapatkan ekstensi file untuk menentukan content type
+        $fileExtension = pathinfo($brosurPath, PATHINFO_EXTENSION);
+
+        // Set header berdasarkan ekstensi file
+        switch ($fileExtension) {
+            case 'pdf':
+                header('Content-Type: application/pdf');
+                break;
+            // Tambahkan case lain jika perlu
+            default:
+                header('Content-Type: application/octet-stream');
+        }
+
+        header('Content-Disposition: attachment; filename="' . basename($brosurPath) . '"');
+        readfile($brosurPath);
+        exit;
+    } else {
+        echo "Maaf, file tidak ditemukan.";
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About PoisonPantry.com</title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <style>
+        /* Custom CSS */
+        body {
+            background-image: url('img/bg.jpg');
+            background-size: cover;
+            background-position: center;
+            background-attachment: fixed;
+        }
+    </style>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #000000;">
+        <a class="navbar-brand" href="index.php">Poison Pantry</a>
+        <!-- Navbar content -->
+    </nav>
+
+    <div class="container mt-5">
+        <h2 class="text-center mb-4">About PoisonPantry.com</h2>
+        <p class="text-center">
+            Embark on Your Mystical Journey! Dive into our collection and find the potion that resonates with your soul. Whether for collection, display, or to simply bask in their enigmatic presence, PoisonPantry.com is your portal to a world of arcane wonder.
+        </p>
+        <!-- Download Brosur Link -->
+        <div class="text-center mt-4">
+            <a href="download_brosur.php?brosur=our_potions" class="btn btn-primary">Download Our Potions Brosur</a>
+        </div>
+    </div>
+
+    <!-- Bootstrap JS, Popper.js, and jQuery -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.9.3/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
